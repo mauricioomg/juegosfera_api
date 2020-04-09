e4b540e82fa67a21d562fff9ddc8821345c233cd
danielchikara <brandonnakama@gmail.com>
2020-04-09 11:48:59 -0500

    fix hash

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -9,43 +9,39 @@ class Product1Serializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    #url = serializers.HyperlinkedIdentityField(view_name="index:user-detail")
+#class UserSerializer(serializers.HyperlinkedModelSerializer):
+#    #url = serializers.HyperlinkedIdentityField(view_name="index:user-detail")
+#
+#    class Meta:
+#        model = User
+#        fields = ['username', 'email', 'password', ]
+#
+#    def validate_username(self, data):
+#        users = User.objects.filter(username = data)
+#        if users.count() != 0 :
+#            raise serializers.ValidationError('Este nombre de usuario ya existe, ingrese uno nuevo')
+#        else:
+#            return data 
+#
+# 
+class UserSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', ]
+    def create(self, validate_data):
+        instance = User()
+        instance.username = validate_data.get('username')
+        instance.email = validate_data.get('email')
+        instance.set_password(validate_data.get('password'))
+        instance.save()
+        return instance
 
     def validate_username(self, data):
         users = User.objects.filter(username = data)
-        if users.count() != 0 :
+        if len(users) != 0 :
             raise serializers.ValidationError('Este nombre de usuario ya existe, ingrese uno nuevo')
         else:
             return data 
-
- 
-#class UserSerializer(serializers.Serializer):
- #   id = serializers.ReadOnlyField()
-  #  first_name = serializers.CharField()
-  #  last_name = serializers.CharField()
-   # username = serializers.CharField()
-    #email = serializers.EmailField()
-    #assword = serializers.CharField()
-
-    #def create(self, validate_data):
-     #   instance = User()
-      #  instance.first_name = validate_data.get('first_name')
-       # instance.last_name = validate_data.get('last_name')
-        #instance.username = validate_data.get('username')
-        #instance.email = validate_data.get('email')
-        #instance.set_password(validate_data.get('password'))
-        #instance.save()
-        #return instance
-
-    #def validate_username(self, data):
-    #    users = User.objects.filter(username = data)
-    #    if len(users) != 0 :
-    #        raise serializers.ValidationError('Este nombre de usuario ya existe, ingrese uno nuevo')
-    #    else:
-    #        return data 
  
